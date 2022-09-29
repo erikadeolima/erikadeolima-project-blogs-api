9d95fc77c8f017641a2ab1c9976d287497bb8aa6
your_erikadeolima <your_erikadeo.lim@hotmail.com>
2022-09-29 13:54:41 -0300

    feat: req14 done

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -14,7 +14,15 @@ const postSearch = async () => {
 };
 
 const postSearchById = async (id) => {
-  const post = await BlogPost.findByPk(id);
+  const post = await BlogPost.findByPk(id, {
+    include: [{
+        model: User, as: 'user', attributes: { exclude: ['password'] },
+    },
+    { 
+        model: Category, as: 'categories', through: { attributes: [] },
+    }],
+});
+if (post === null) { throw errorGenerate(404, 'Post does not exist'); }
   return post;
 };
 
